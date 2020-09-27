fe2e977d013a22a23f2050a80cfb73d8fa430ae4
hidesan4869 <t1123.ikik@gmail.com>
2020-09-27 21:38:25 +0900

    指定されていた修正を完了

--- src/asset/js/app.js
+++ src/asset/js/app.js
@@ -1,7 +1,9 @@
-(function($) {
-  $(function () {
-    $('#nav-toggle').on('click', function() {
-      $('body').toggleClass('open');
-    });
-  });
-})(jQuery);
+$('.js-nav__button').on('click',function(){
+  if( $(this).hasClass('active') ){
+    $(this).removeClass('active');
+    $('.js-nav__wrap').addClass('close').removeClass('open');
+  }else {
+    $(this).addClass('active');
+    $('.js-nav__wrap').addClass('open').removeClass('close'); 
+  }
+});
